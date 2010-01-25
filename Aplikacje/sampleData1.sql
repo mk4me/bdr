03b195a0408705cfcf217967fc91913d9c3d3d03
habela <habela@5173494c-6dae-45e1-bfb1-e08744c75a4d>
2010-01-25 08:51:37 +0000

    Current SQL scripts - first commit
    git-svn-id: https://localhost:8443/svn/bdr@15 5173494c-6dae-45e1-bfb1-e08744c75a4d

---
+++ Aplikacje/sampleData1.sql
@@ -0,0 +1,29 @@
+-- select * from Uzytkownik
+
+-- insert into Uzytkownik (Login, Haslo, Imie, Nazwisko) values ( 'habela', 'password', 'Piotr', 'Habela')
+
+-- select * from Performer
+
+-- insert into Performer (Imie, Nazwisko) values ( 'Jan', 'Kowalski')
+-- insert into Performer (Imie, Nazwisko) values ( 'Anna', 'Nowak')
+-- select * from Laboratorium
+-- insert into Laboratorium (Nazwa) values ('PJWSTK')
+-- select * from Rodzaj_ruchu
+-- insert into Rodzaj_ruchu (Nazwa) values ('walk')
+-- insert into Rodzaj_ruchu (Nazwa) values ('run')
+-- select * from Sesja
+ --insert into Sesja (IdUzytkownik, IdLaboratorium, IdRodzaj_ruchu, IdPerformer, Data, Opis_sesji) values ( 
+	--(select IdUzytkownik from Uzytkownik where Nazwisko='habela'), 
+	--(select IdLaboratorium from Laboratorium where Nazwa='PJWSTK'), 
+	--(select IdRodzaj_ruchu from Rodzaj_ruchu where Nazwa='walk'), 
+	--(select IdPerformer from Performer where Nazwisko='Kowalski'), 
+	--GETDATE(), 
+	--'sesja testowa nr 1' )
+--insert into Sesja (IdUzytkownik, IdLaboratorium, IdRodzaj_ruchu, IdPerformer, Data, Opis_sesji) values ( 
+--	(select IdUzytkownik from Uzytkownik where Nazwisko='habela'), 
+--	(select IdLaboratorium from Laboratorium where Nazwa='PJWSTK'), 
+--	(select IdRodzaj_ruchu from Rodzaj_ruchu where Nazwa='run'), 
+--	(select IdPerformer from Performer where Nazwisko='Kowalski'), 
+--	GETDATE(), 
+--	'sesja testowa nr 2' )
+select * from Sesja
