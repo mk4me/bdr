c05d07a1b372ed8b2ab6838ba0bc97d9078c2397
WiktorF <WiktorF@5173494c-6dae-45e1-bfb1-e08744c75a4d>
2010-02-09 12:28:25 +0000

    Wysylanie pliku przez aplet.
    git-svn-id: https://localhost:8443/svn/bdr@39 5173494c-6dae-45e1-bfb1-e08744c75a4d

--- Aplikacje/Prototyp/trunk/src/motion/applet/dialogs/UploadDialog.java
+++ Aplikacje/Prototyp/trunk/src/motion/applet/dialogs/UploadDialog.java
@@ -17,12 +17,16 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
+import motion.database.DatabaseConnection;
+
 public class UploadDialog extends JDialog {
 	private static String UPLOAD_TITLE = "Upload session file";
 	private static String UPLOAD_FILE = "Upload";
 	private static String FILE_PATH = "File:";
 	private static String CANCEL_UPLOAD = "Cancel";
 	private static String WELCOME_MESSAGE = "Choose a file to upload.";
+	private static String MISSING_FILE_PATH_MESSAGE = "No file selected.";
+	private static String PRESS_UPLOAD_MESSAGE = "Press Upload to send file.";
 	private static String BROWSE = "Browse";
 	private JButton browseButton;
 	private JButton uploadButton;
@@ -30,9 +34,14 @@ public class UploadDialog extends JDialog {
 	private JLabel messageLabel;
 	private JTextField filePathText;
 	
+	private DatabaseConnection databaseConnection;
 	
 	public UploadDialog() {
 		super((JFrame) null, UPLOAD_TITLE, true);
+		
+		databaseConnection = DatabaseConnection.getInstance();
+		databaseConnection.setWSCredentials("applet", "motion#motion2X", "pjwstk");
+		databaseConnection.setFTPSCredentials("dbpawell", "testUser", "testUser");
 		
 		this.setSize(380, 200);
 		this.setLocation(200, 200);
@@ -96,14 +105,26 @@ public class UploadDialog extends JDialog {
 				fileChooser.setFileFilter(new FileNameExtensionFilter(".c3d session file", "c3d"));
 				fileChooser.showOpenDialog(UploadDialog.this);
 				File file = fileChooser.getSelectedFile();
-				if (file != null)
+				if (file != null) {
 					UploadDialog.this.filePathText.setText(file.toString());
+					UploadDialog.this.validateResult();
+				}
 			}
 		});
 		
 		this.uploadButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				// Upload file
+				if (UploadDialog.this.validateResult() == true) {
+					try {
+						UploadDialog.this.databaseConnection.uploadFile(1, "Plik wgrany przez applet", UploadDialog.this.filePathText.getText());
+					} catch (Exception e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					
+					UploadDialog.this.setVisible(false);
+					UploadDialog.this.dispose();
+				}
 			}
 		});
 		
@@ -114,5 +135,22 @@ public class UploadDialog extends JDialog {
 				UploadDialog.this.dispose();
 			}
 		});
+	}
+	
+	private String getFilePath() {
+		
+		return this.filePathText.getText();
+	}
+	
+	private boolean validateResult() {
+		if (this.getFilePath().equals("")) {
+			this.messageLabel.setText(MISSING_FILE_PATH_MESSAGE);
+			
+			return false;
+		}
+		
+		this.messageLabel.setText(PRESS_UPLOAD_MESSAGE);
+		
+		return true;
 	}
 }
