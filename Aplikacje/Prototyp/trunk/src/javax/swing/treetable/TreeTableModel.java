e3b7a422fcd2b66dab8463e390f4ee29ff404346
kaczmarski <kaczmarski@5173494c-6dae-45e1-bfb1-e08744c75a4d>
2010-05-14 08:52:25 +0000

    Poprawki w JTreeTable. Zobaczymy czy zadziałają.
    git-svn-id: https://localhost:8443/svn/bdr@193 5173494c-6dae-45e1-bfb1-e08744c75a4d

--- Aplikacje/Prototyp/trunk/src/javax/swing/treetable/TreeTableModel.java
+++ Aplikacje/Prototyp/trunk/src/javax/swing/treetable/TreeTableModel.java
@@ -1,42 +1,37 @@
 package javax.swing.treetable;
+
 /*
- * %W% %E%
+
+ * TreeTableModel.java
+ * 
+ * Copyright 1998 Sun Microsystems, Inc.  All Rights Reserved.
  *
- * Copyright 1997, 1998 Sun Microsystems, Inc. All Rights Reserved.
- * 
- * Redistribution and use in source and binary forms, with or
- * without modification, are permitted provided that the following
- * conditions are met:
- * 
- * - Redistributions of source code must retain the above copyright
- *   notice, this list of conditions and the following disclaimer. 
- *   
- * - Redistribution in binary form must reproduce the above
- *   copyright notice, this list of conditions and the following
- *   disclaimer in the documentation and/or other materials
- *   provided with the distribution. 
- *   
- * Neither the name of Sun Microsystems, Inc. or the names of
- * contributors may be used to endorse or promote products derived
- * from this software without specific prior written permission.  
- * 
- * This software is provided "AS IS," without a warranty of any
- * kind. ALL EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND
- * WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY
- * EXCLUDED. SUN AND ITS LICENSORS SHALL NOT BE LIABLE FOR ANY
- * DAMAGES OR LIABILITIES SUFFERED BY LICENSEE AS A RESULT OF OR
- * RELATING TO USE, MODIFICATION OR DISTRIBUTION OF THIS SOFTWARE OR
- * ITS DERIVATIVES. IN NO EVENT WILL SUN OR ITS LICENSORS BE LIABLE 
- * FOR ANY LOST REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT,   
- * SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER  
- * CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF 
- * THE USE OF OR INABILITY TO USE THIS SOFTWARE, EVEN IF SUN HAS 
- * BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
- * 
- * You acknowledge that this software is not designed, licensed or
- * intended for use in the design, construction, operation or
- * maintenance of any nuclear facility.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *   - Neither the name of Sun Microsystems nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 import javax.swing.tree.TreeModel;
@@ -48,8 +43,6 @@ import javax.swing.tree.TreeModel;
  * a TableModel, has a name and a type associated with it. Each node in 
  * the TreeTableModel can return a value for each of the columns and 
  * set that value if isCellEditable() returns true. 
- *
- * @version %I% %G%
  *
  * @author Philip Milne 
  * @author Scott Violet
@@ -89,4 +82,3 @@ public interface TreeTableModel extends TreeModel
      */
     public void setValueAt(Object aValue, Object node, int column);
 }
-
--- Aplikacje/Prototyp/trunk/src/javax/swing/treetable/TreeTableModelAdapter.java
+++ Aplikacje/Prototyp/trunk/src/javax/swing/treetable/TreeTableModelAdapter.java
@@ -1,49 +1,47 @@
 package javax.swing.treetable;
+
 /*
- * %W% %E%
+ * @(#)TreeTableModelAdapter.java       1.2 98/10/27
  *
- * Copyright 1997, 1998 Sun Microsystems, Inc. All Rights Reserved.
- * 
- * Redistribution and use in source and binary forms, with or
- * without modification, are permitted provided that the following
- * conditions are met:
- * 
- * - Redistributions of source code must retain the above copyright
- *   notice, this list of conditions and the following disclaimer. 
- *   
- * - Redistribution in binary form must reproduce the above
- *   copyright notice, this list of conditions and the following
- *   disclaimer in the documentation and/or other materials
- *   provided with the distribution. 
- *   
- * Neither the name of Sun Microsystems, Inc. or the names of
- * contributors may be used to endorse or promote products derived
- * from this software without specific prior written permission.  
- * 
- * This software is provided "AS IS," without a warranty of any
- * kind. ALL EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND
- * WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY
- * EXCLUDED. SUN AND ITS LICENSORS SHALL NOT BE LIABLE FOR ANY
- * DAMAGES OR LIABILITIES SUFFERED BY LICENSEE AS A RESULT OF OR
- * RELATING TO USE, MODIFICATION OR DISTRIBUTION OF THIS SOFTWARE OR
- * ITS DERIVATIVES. IN NO EVENT WILL SUN OR ITS LICENSORS BE LIABLE 
- * FOR ANY LOST REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT,   
- * SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER  
- * CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF 
- * THE USE OF OR INABILITY TO USE THIS SOFTWARE, EVEN IF SUN HAS 
- * BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
- * 
- * You acknowledge that this software is not designed, licensed or
- * intended for use in the design, construction, operation or
- * maintenance of any nuclear facility.
+ * Copyright 1997, 1998 Sun Microsystems, Inc.  All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *   - Neither the name of Sun Microsystems nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+ 
 
 import javax.swing.JTree;
-import javax.swing.event.TreeExpansionEvent;
-import javax.swing.event.TreeExpansionListener;
+import javax.swing.SwingUtilities;
 import javax.swing.table.AbstractTableModel;
 import javax.swing.tree.TreePath;
+import javax.swing.event.TreeExpansionEvent;
+import javax.swing.event.TreeExpansionListener;
+import javax.swing.event.TreeModelEvent;
+import javax.swing.event.TreeModelListener;
 
 /**
  * This is a wrapper class takes a TreeTableModel and implements 
@@ -51,13 +49,11 @@ import javax.swing.tree.TreePath;
  * all of the event dispatching support provided by the superclass: 
  * the AbstractTableModel. 
  *
- * @version %I% %G%
+ * @version 1.2 10/27/98
  *
  * @author Philip Milne
  * @author Scott Violet
  */
-
-
 public class TreeTableModelAdapter extends AbstractTableModel
 {
     JTree tree;
@@ -67,43 +63,65 @@ public class TreeTableModelAdapter extends AbstractTableModel
         this.tree = tree;
         this.treeTableModel = treeTableModel;
 
-	tree.addTreeExpansionListener(new TreeExpansionListener() {
-	    // Don't use fireTableRowsInserted() here; 
-	    // the selection model would get  updated twice. 
-	    public void treeExpanded(TreeExpansionEvent event) {  
-	      fireTableDataChanged(); 
-	    }
+        tree.addTreeExpansionListener(new TreeExpansionListener() {
+            // Don't use fireTableRowsInserted() here; the selection model
+            // would get updated twice. 
+            public void treeExpanded(TreeExpansionEvent event) {  
+              fireTableDataChanged(); 
+            }
             public void treeCollapsed(TreeExpansionEvent event) {  
-	      fireTableDataChanged(); 
-	    }
-	});
+              fireTableDataChanged(); 
+            }
+        });
+
+        // Install a TreeModelListener that can update the table when
+        // tree changes. We use delayedFireTableDataChanged as we can
+        // not be guaranteed the tree will have finished processing
+        // the event before us.
+        treeTableModel.addTreeModelListener(new TreeModelListener() {
+            public void treeNodesChanged(TreeModelEvent e) {
+                delayedFireTableDataChanged();
+            }
+
+            public void treeNodesInserted(TreeModelEvent e) {
+                delayedFireTableDataChanged();
+            }
+
+            public void treeNodesRemoved(TreeModelEvent e) {
+                delayedFireTableDataChanged();
+            }
+
+            public void treeStructureChanged(TreeModelEvent e) {
+                delayedFireTableDataChanged();
+            }
+        });
     }
 
-  // Wrappers, implementing TableModel interface. 
+    // Wrappers, implementing TableModel interface. 
 
     public int getColumnCount() {
-	return treeTableModel.getColumnCount();
+        return treeTableModel.getColumnCount();
     }
 
     public String getColumnName(int column) {
-	return treeTableModel.getColumnName(column);
+        return treeTableModel.getColumnName(column);
     }
 
     public Class getColumnClass(int column) {
-	return treeTableModel.getColumnClass(column);
+        return treeTableModel.getColumnClass(column);
     }
 
     public int getRowCount() {
-	return tree.getRowCount();
+        return tree.getRowCount();
     }
 
     protected Object nodeForRow(int row) {
-	TreePath treePath = tree.getPathForRow(row);
-	return treePath.getLastPathComponent();         
+        TreePath treePath = tree.getPathForRow(row);
+        return treePath.getLastPathComponent();         
     }
 
     public Object getValueAt(int row, int column) {
-	return treeTableModel.getValueAt(nodeForRow(row), column);
+        return treeTableModel.getValueAt(nodeForRow(row), column);
     }
 
     public boolean isCellEditable(int row, int column) {
@@ -111,8 +129,19 @@ public class TreeTableModelAdapter extends AbstractTableModel
     }
 
     public void setValueAt(Object value, int row, int column) {
-	treeTableModel.setValueAt(value, nodeForRow(row), column);
+        treeTableModel.setValueAt(value, nodeForRow(row), column);
+    }
+
+    /**
+     * Invokes fireTableDataChanged after all the pending events have been
+     * processed. SwingUtilities.invokeLater is used to handle this.
+     */
+    protected void delayedFireTableDataChanged() {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                fireTableDataChanged();
+            }
+        });
     }
 }
-
 
