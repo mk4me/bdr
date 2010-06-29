fa626b3f1a87b723ce7ab54e4772f3d263eb62b3
habela <habela@5173494c-6dae-45e1-bfb1-e08744c75a4d>
2010-06-29 07:17:35 +0000

    Unikniecie tworzenia duplikatow przy zmianie przydzialu uprawnien do sesji
    git-svn-id: https://localhost:8443/svn/bdr@315 5173494c-6dae-45e1-bfb1-e08744c75a4d

--- Aplikacje/SQL_scripts/storedProcedures2.sql
+++ Aplikacje/SQL_scripts/storedProcedures2.sql
@@ -672,10 +672,21 @@ go
 create procedure set_session_privileges (@granting_user_login varchar(30), @granted_user_login varchar(30), @sess_id int, @write bit)
 as
 begin
-
+	declare @granted_user int;
 	if (select COUNT(*) from user_sessions_by_login(@granting_user_login) where IdSesja = @sess_id)<>1 RAISERROR ('Session not owned by granting user', 12, 1 )
 	else
-	insert into Uprawnienia_sesja ( IdSesja, IdUzytkownik, Zapis) values (@sess_id, dbo.identify_user(@granted_user_login), @write);
+	begin
+		set @granted_user = dbo.identify_user(@granted_user_login);
+		if @granted_user is NULL 
+			begin
+				RAISERROR ('Granted user does not exist', 12, 1 )
+				return;
+			end
+		else
+		update Uprawnienia_sesja set Zapis = @write where IdSesja = @sess_id and IdUzytkownik = @granted_user;
+		if @@ROWCOUNT = 0
+		insert into Uprawnienia_sesja ( IdSesja, IdUzytkownik, Zapis) values (@sess_id, @granted_user, @write);
+	end
 end
 go
 
