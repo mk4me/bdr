2c274b726417ae366959a38398a9ec2567159a75
kaczmarski <kaczmarski@5173494c-6dae-45e1-bfb1-e08744c75a4d>
2010-07-09 06:55:17 +0000

    Wersja gotowa dla db-bdr
    git-svn-id: https://localhost:8443/svn/bdr@361 5173494c-6dae-45e1-bfb1-e08744c75a4d

--- Aplikacje/Prototyp/trunk/src/motion/applet/dialogs/LoginDialog.java
+++ Aplikacje/Prototyp/trunk/src/motion/applet/dialogs/LoginDialog.java
@@ -97,10 +97,10 @@ public class LoginDialog extends BasicDialog {
 		this.addToButtonPanel(cancelButton);
 		
 		// Initial text
-		loginText.setText( "applet_user" );
-		passwordText.setText( "aplet4Motion" );
-		domainText.setText("db-bdr");
-		//domainText.setText("pjwstk");
+		//loginText.setText( "applet_user" );
+		//passwordText.setText( "aplet4Motion" );
+		//domainText.setText("db-bdr");
+		domainText.setText("pjwstk");
 		
 	}
 	
@@ -119,7 +119,7 @@ public class LoginDialog extends BasicDialog {
 					protected Void doInBackground() throws InterruptedException {
 						LoginDialog.this.loginButton.setEnabled(false);
 						WebServiceInstance.getDatabaseConnection().setWSCredentials( loginText.getText().trim(), passwordText.getText(), domainText.getText());
-						WebServiceInstance.getDatabaseConnection().setFTPSCredentials("dbpawell.pjwstk.edu.pl", "testUser", "testUser");
+						WebServiceInstance.getDatabaseConnection().setFTPSCredentials("db-bdr.pjwstk.edu.pl", "testUser", "testUser");
 						
 						return null;
 					}
