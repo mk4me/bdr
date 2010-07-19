cde01a73f290b1878cb3a97222fdaaf5b504b15a
WiktorF <WiktorF@5173494c-6dae-45e1-bfb1-e08744c75a4d>
2010-07-19 16:21:26 +0000

    Usuniety niepotrzebny kod przy uruchamianiu.
    git-svn-id: https://localhost:8443/svn/bdr@408 5173494c-6dae-45e1-bfb1-e08744c75a4d

--- Aplikacje/Prototyp/trunk/src/motion/applet/toolbars/AppletToolBar.java
+++ Aplikacje/Prototyp/trunk/src/motion/applet/toolbars/AppletToolBar.java
@@ -1,22 +1,14 @@
 package motion.applet.toolbars;
 
 import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JComboBox;
 import javax.swing.JToolBar;
 
 import motion.applet.MotionAppletFrame;
-import motion.applet.database.TableName;
-import motion.applet.database.TableNamesInstance;
 
 public class AppletToolBar extends JToolBar {
-	private TableName[] tableNames = {TableNamesInstance.PERFORMER,
-			TableNamesInstance.SESSION};
-			//TableNamesInstance.TRIAL,
-			//TableNamesInstance.PATIENT};
-	private JComboBox tableComboBox = new JComboBox(tableNames);
 	private String[] labs = {"Lab 0", "Lab 1"};
 	private JComboBox labComboBox = new JComboBox(labs);
 	private static int labId = 1;
@@ -27,23 +19,9 @@ public class AppletToolBar extends JToolBar {
 		FlowLayout appletToolBarLayout = new FlowLayout();
 		appletToolBarLayout.setAlignment(FlowLayout.LEADING);
 		this.setLayout(appletToolBarLayout);
-		//JButton showTableButton = new JButton("Show");
-		//this.add(showTableButton);
-		//this.add(tableComboBox);
 		
 		labComboBox.setSelectedIndex(labId);
 		this.add(labComboBox);
-		
-		labComboBox.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				labId = AppletToolBar.this.labComboBox.getSelectedIndex();
-				//AppletToolBar.this.tableComboBox.setSelectedIndex(AppletToolBar.this.tableComboBox.getSelectedIndex());
-			}
-		});
-	}
-	
-	public void addTableComboBoxListener(ActionListener actionListener) {
-		this.tableComboBox.addActionListener(actionListener);
 	}
 	
 	public void addLabComboBoxListener(ActionListener actionListener) {
