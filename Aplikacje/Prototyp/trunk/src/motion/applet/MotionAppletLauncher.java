0a279c8071e6a4aa76e5d18f5b5170a756f4743d
WiktorF <WiktorF@5173494c-6dae-45e1-bfb1-e08744c75a4d>
2010-07-21 15:05:20 +0000

    Mozliwosc wyboru jezyka.
    git-svn-id: https://localhost:8443/svn/bdr@416 5173494c-6dae-45e1-bfb1-e08744c75a4d

--- Aplikacje/Prototyp/trunk/src/motion/applet/MotionAppletLauncher.java
+++ Aplikacje/Prototyp/trunk/src/motion/applet/MotionAppletLauncher.java
@@ -7,14 +7,18 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JApplet;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class MotionAppletLauncher extends JApplet {
+	private JComboBox languageComboBox = new JComboBox();
+	private static String ENGLISH = "English";
+	private static String POLISH = "Polish";
 	
 	@Override
 	public void init() {
-		this.setSize(150, 80);
+		this.setSize(230, 80);
 		
 		// Set language
 		//Messages.setLanguagePolish();
@@ -22,7 +26,7 @@ public class MotionAppletLauncher extends JApplet {
 		
 		JPanel titlePanel = new JPanel();
 		titlePanel.setBackground(Color.white);
-		JLabel titleLabel = new JLabel("<html><p align=center>Motion client<br>(click button to start)</p></html>");
+		JLabel titleLabel = new JLabel("<html><p align=center>Motion client<br>(click button to start, select language)</p></html>");
 		titlePanel.add(titleLabel);
 		this.getContentPane().add(titlePanel, BorderLayout.NORTH);
 		
@@ -30,12 +34,28 @@ public class MotionAppletLauncher extends JApplet {
 		buttonPanel.setBackground(Color.white);
 		JButton launcherButton = new JButton("Start applet");
 		buttonPanel.add(launcherButton);
+		
+		languageComboBox.addItem(ENGLISH);
+		languageComboBox.addItem(POLISH);
+		buttonPanel.add(languageComboBox);
+		
 		this.getContentPane().add(buttonPanel, BorderLayout.CENTER);
 		
 		launcherButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-
+				
 				MotionAppletFrame.main(new String[0]);
+			}
+		});
+		
+		languageComboBox.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				if (languageComboBox.getSelectedItem().equals(ENGLISH)) {
+					Messages.setLanguageEnglish();
+				} else if (languageComboBox.getSelectedItem().equals(POLISH)) {
+					Messages.setLanguagePolish();
+				}
 			}
 		});
 	}
