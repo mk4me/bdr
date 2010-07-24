18c428b99d465bafd14b132193fe86b063b60bd6
kaczmarski <kaczmarski@5173494c-6dae-45e1-bfb1-e08744c75a4d>
2010-07-24 09:08:31 +0000

    Przekazanie uprawniń sesji do bazy
    git-svn-id: https://localhost:8443/svn/bdr@432 5173494c-6dae-45e1-bfb1-e08744c75a4d

--- Aplikacje/DatabaseConnection/src/motion/database/ws/SessionPrivilegesSetter.java
+++ Aplikacje/DatabaseConnection/src/motion/database/ws/SessionPrivilegesSetter.java
@@ -78,13 +78,25 @@ public class SessionPrivilegesSetter {
 								sessionID, 
 								(Boolean)((EntityAttribute)actual.get( UserPrivilegesStaticAttributes.canWrite)).value );
 					else
+					{
 						db.grantSessionPrivileges( 
 								(String)((EntityAttribute)wanted.get( UserPrivilegesStaticAttributes.login)).value, 
 								(String)((EntityAttribute)wanted.get( UserPrivilegesStaticAttributes.login)).value, 
 								sessionID, 
 								(Boolean)((EntityAttribute)wanted.get( UserPrivilegesStaticAttributes.canWrite)).value );
+						userPrivileges.remove( wanted );
+					}
 				}
-			
+				for (UserPrivileges toBeAdded : userPrivileges)
+				{
+					db.grantSessionPrivileges( 
+							(String)((EntityAttribute)toBeAdded.get( UserPrivilegesStaticAttributes.login)).value, 
+							(String)((EntityAttribute)toBeAdded.get( UserPrivilegesStaticAttributes.login)).value, 
+							sessionID, 
+							(Boolean)((EntityAttribute)toBeAdded.get( UserPrivilegesStaticAttributes.canWrite)).value );
+					
+				}
+
 			}
 		}
 	}
