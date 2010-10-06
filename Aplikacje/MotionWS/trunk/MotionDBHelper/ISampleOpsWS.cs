cb6fec91fe498256a4b5b3161433978449d060c9
habela <habela@5173494c-6dae-45e1-bfb1-e08744c75a4d>
2010-10-06 14:59:00 +0000

    testowa usluga SampleOps
    git-svn-id: https://localhost:8443/svn/bdr@654 5173494c-6dae-45e1-bfb1-e08744c75a4d

---
+++ Aplikacje/MotionWS/trunk/MotionDBHelper/ISampleOpsWS.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Xml;
+
+namespace MotionDBWebServices
+{
+    // NOTE: If you change the interface name "ISampleOpsWS" here, you must also update the reference to "ISampleOpsWS" in Web.config.
+    [ServiceContract(Namespace = "http://ruch.bytom.pjwstk.edu.pl/MotionDB/SampleOpsService"), XmlSerializerFormat(Style = OperationFormatStyle.Document, Use = OperationFormatUse.Literal)]
+    public interface ISampleOpsWS
+    {
+        //[OperationContract]
+        //[FaultContract(typeof(QueryException))]
+        //XmlElement ListLabSessionsWithAttributesXML(int labID);
+
+
+        [OperationContract]
+        [FaultContract(typeof(QueryException))]
+        XmlElement ListPerformersXML();
+    }
+}
