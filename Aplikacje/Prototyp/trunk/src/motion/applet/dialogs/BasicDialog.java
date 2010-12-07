c18ca6eb18be2df4b6f0b8ad8ff900834cea6a67
WiktorF <WiktorF@5173494c-6dae-45e1-bfb1-e08744c75a4d>
2010-12-07 17:17:27 +0000

    Obsluga wielo-linjkowych komunikatow w dialogach.
    git-svn-id: https://localhost:8443/svn/bdr@819 5173494c-6dae-45e1-bfb1-e08744c75a4d

--- Aplikacje/Prototyp/trunk/src/motion/applet/dialogs/BasicDialog.java
+++ Aplikacje/Prototyp/trunk/src/motion/applet/dialogs/BasicDialog.java
@@ -3,23 +3,28 @@ package motion.applet.dialogs;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Insets;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSeparator;
+import javax.swing.JTextPane;
 import javax.swing.SwingConstants;
 import javax.swing.border.Border;
 import javax.swing.border.EtchedBorder;
+import javax.swing.text.Style;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyleContext;
 
 import motion.applet.MotionApplet;
 
 public abstract class BasicDialog extends JDialog {
-	protected JLabel messageLabel;
+	protected JTextPane messageLabel;
 	private JPanel buttonPanel = new JPanel();
 	
 	public BasicDialog(String title, String message) {
@@ -41,10 +46,21 @@ public abstract class BasicDialog extends JDialog {
 	// Welcome message and form validation messages.
 	private void createMessageArea(String message) {
 		JPanel messagePanel = new JPanel();
+		messagePanel.setLayout(new BoxLayout(messagePanel, BoxLayout.Y_AXIS));
 		messagePanel.setBackground(Color.white);
 		Border border = BorderFactory.createEtchedBorder(EtchedBorder.LOWERED);
 		messagePanel.setBorder(border);
-		this.messageLabel = new JLabel(message);
+		messageLabel = new JTextPane();
+		messageLabel.setText(message);
+		messageLabel.setEditable(false);
+		messageLabel.setMargin(new Insets(10, 10, 10, 10));
+		Font font = messageLabel.getFont().deriveFont(Font.BOLD);
+		messageLabel.setFont(font);
+		
+		Style doc = messageLabel.getStyle(StyleContext.DEFAULT_STYLE);
+		StyleConstants.setAlignment(doc, StyleConstants.ALIGN_CENTER);
+		messageLabel.setParagraphAttributes(doc, true);
+		
 		messagePanel.add(this.messageLabel);
 		this.add(messagePanel, BorderLayout.PAGE_START);
 	}
