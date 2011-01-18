6d8e44d384fffcbf4a7701d1ad4fa6d1d46fbf48
WiktorF <WiktorF@5173494c-6dae-45e1-bfb1-e08744c75a4d>
2011-01-18 13:03:22 +0000

    Dodana metoda getById do MeasurementConfiguration.
    git-svn-id: https://localhost:8443/svn/bdr@857 5173494c-6dae-45e1-bfb1-e08744c75a4d

--- Aplikacje/DatabaseConnection/src/motion/database/model/EntityKind.java
+++ Aplikacje/DatabaseConnection/src/motion/database/model/EntityKind.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
 
@@ -12,22 +11,23 @@ import motion.Messages;
 import motion.database.DatabaseConnection;
 import motion.database.DbElementsList;
 import motion.database.ws.ConnectionTools2;
-import motion.database.ws.basicQueriesServiceWCF.FileWithAttributesList;
 import motion.database.ws.basicQueriesServiceWCF.IBasicQueriesWS;
 import motion.database.ws.basicQueriesServiceWCF.IBasicQueriesWSGetFileDataByIdXMLQueryExceptionFaultFaultMessage;
 import motion.database.ws.basicQueriesServiceWCF.IBasicQueriesWSGetMeasurementByIdXMLQueryExceptionFaultFaultMessage;
+import motion.database.ws.basicQueriesServiceWCF.IBasicQueriesWSGetMeasurementConfigurationByIdXMLQueryExceptionFaultFaultMessage;
 import motion.database.ws.basicQueriesServiceWCF.IBasicQueriesWSGetPerformerByIdXMLQueryExceptionFaultFaultMessage;
 import motion.database.ws.basicQueriesServiceWCF.IBasicQueriesWSGetPerformerConfigurationByIdXMLQueryExceptionFaultFaultMessage;
 import motion.database.ws.basicQueriesServiceWCF.IBasicQueriesWSGetSessionByIdXMLQueryExceptionFaultFaultMessage;
 import motion.database.ws.basicQueriesServiceWCF.IBasicQueriesWSGetTrialByIdXMLQueryExceptionFaultFaultMessage;
+import motion.database.ws.basicQueriesServiceWCF.MeasurementConfDetailsWithAttributes;
 import motion.database.ws.basicQueriesServiceWCF.MeasurementDetailsWithAttributes;
 import motion.database.ws.basicQueriesServiceWCF.PerformerConfDetailsWithAttributes;
 import motion.database.ws.basicQueriesServiceWCF.PerformerDetailsWithAttributes;
 import motion.database.ws.basicQueriesServiceWCF.SessionDetailsWithAttributes;
 import motion.database.ws.basicQueriesServiceWCF.TrialDetailsWithAttributes;
-import motion.database.ws.basicQueriesServiceWCF.FileWithAttributesList.FileDetailsWithAttributes;
 import motion.database.ws.basicQueriesServiceWCF.GetFileDataByIdXMLResponse.GetFileDataByIdXMLResult;
 import motion.database.ws.basicQueriesServiceWCF.GetMeasurementByIdXMLResponse.GetMeasurementByIdXMLResult;
+import motion.database.ws.basicQueriesServiceWCF.GetMeasurementConfigurationByIdXMLResponse.GetMeasurementConfigurationByIdXMLResult;
 import motion.database.ws.basicQueriesServiceWCF.GetPerformerByIdXMLResponse.GetPerformerByIdXMLResult;
 import motion.database.ws.basicQueriesServiceWCF.GetPerformerConfigurationByIdXMLResponse.GetPerformerConfigurationByIdXMLResult;
 import motion.database.ws.basicQueriesServiceWCF.GetSessionByIdXMLResponse.GetSessionByIdXMLResult;
@@ -353,6 +353,25 @@ public enum EntityKind {
 		public void setEntityAttribute(IBasicUpdatesWS port, int ID,
 				EntityAttribute a, boolean update) throws Exception {
 			port.setMeasurementConfAttribute(ID, a.name, a.value.toString(), update);
+		}
+		
+		@Override
+		public MeasurementConfiguration getByID(IBasicQueriesWS port, int id) throws Exception {
+			try{
+				GetMeasurementConfigurationByIdXMLResult result = port.getMeasurementConfigurationByIdXML(id);
+				MeasurementConfDetailsWithAttributes s = result.getMeasurementConfDetailsWithAttributes();
+		
+				return ConnectionTools2.transformMeasurementConfigurationDetails(s);
+			}
+			catch(IBasicQueriesWSGetMeasurementConfigurationByIdXMLQueryExceptionFaultFaultMessage e)
+			{
+				DatabaseConnection.log.log( Level.SEVERE, e.getFaultInfo().getDetails().getValue(), e );
+				throw new Exception( e.getFaultInfo().getDetails().getValue(), e ); 
+			}
+			finally
+			{
+				ConnectionTools2.finalizeCall();
+			}
 		}
 		
 		@Override
