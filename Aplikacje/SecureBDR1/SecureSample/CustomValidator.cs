e608267b0e227d3054af2e98badb259cf12eaaa6
habela <habela@5173494c-6dae-45e1-bfb1-e08744c75a4d>
2012-01-25 14:21:06 +0000

    git-svn-id: https://localhost:8443/svn/bdr@936 5173494c-6dae-45e1-bfb1-e08744c75a4d

---
+++ Aplikacje/SecureBDR1/SecureSample/CustomValidator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ServiceModel;
+using System.IdentityModel.Selectors;
+using System.IdentityModel.Tokens;
+
+namespace SecureSample
+{
+    public class CustomValidator : System.IdentityModel.Selectors.UserNamePasswordValidator
+    {
+        public override void Validate(string userName, string password)
+        {
+            if (userName == null || password == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (!(userName == "hml" && password == "hml"))
+            {
+                throw new SecurityTokenException("Unknown Username or Password");
+            }
+        }
+
+    }
+}
---
+++ Aplikacje/SecureBDR1/SecureSample/Service1.svc.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace SecureSample
+{
+    // NOTE: If you change the class name "Service1" here, you must also update the reference to "Service1" in Web.config and in the associated .svc file.
+    [ServiceBehavior(Namespace = "http://ruch.bytom.pjwstk.edu.pl/MotionDB/SecureSample/Service1")]
+    public class Service1 : IService1
+    {
+        public string CallMe(int value)
+        {
+
+
+            string username = OperationContext.Current.ServiceSecurityContext.PrimaryIdentity.Name;
+            return "Hello user \""+username+"\" you entered the value: "+value;
+        }
+        /*
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        } */
+    }
+}
