c47f9cd291e0a158ae7b0bb60d106768d046fe4e
habela <habela@5173494c-6dae-45e1-bfb1-e08744c75a4d>
2012-05-26 18:29:19 +0000

    git-svn-id: https://localhost:8443/svn/bdr@968 5173494c-6dae-45e1-bfb1-e08744c75a4d

--- Aplikacje/SQL_scripts/20120504_UserManagementBDR.sql
+++ Aplikacje/SQL_scripts/20120504_UserManagementBDR.sql
@@ -40,4 +40,83 @@ select
  for XML RAW ('UserAssignments'), TYPE;
 go
 
-exec get_user_assignments
+
+-- last rev. 2012-04-05
+-- Error codes:
+-- 1 user not found
+-- 2 user group not found
+create procedure assign_user_to_user_group (  @user_id int, @user_group_id int, @assigned bit, @result int OUTPUT)
+as
+begin
+	set @result = 0;
+	if not exists ( select * from Uzytkownik where IdUzytkownik = @user_id ) 
+	begin
+		set @result = 1;
+		return;
+	end;
+	if not exists ( select * from Grupa_uzytkownikow where IdGrupa_uzytkownikow = @user_group_id ) 
+	begin
+		set @result = 2;
+		return;
+	end;
+	if @assigned = 0
+		delete from Uzytkownik_grupa_uzytkownikow where IdGrupa_uzytkownikow = @user_group_id and IdUzytkownik = @user_id;
+	else
+	if not exists ( select * from	Uzytkownik_grupa_uzytkownikow where IdGrupa_uzytkownikow = @user_group_id and IdUzytkownik = @user_id ) 
+		insert into Uzytkownik_grupa_uzytkownikow ( IdGrupa_uzytkownikow, IdUzytkownik ) values ( @user_group_id, @user_id );
+end;
+go
+
+-- last rev. 2012-04-05
+-- Error codes:
+-- 1 session not found
+-- 2 session group not found
+-- 3 obligatory parameter empty (length 0)
+create procedure assign_session_to_session_group (  @session_id int, @session_group_id int, @assigned bit, @result int OUTPUT)
+as
+begin
+	set @result = 0;
+	if not exists ( select * from Sesja where IdSesja = @session_id ) 
+	begin
+		set @result = 1;
+		return;
+	end;
+	if not exists ( select * from Grupa_sesji where IdGrupa_sesji= @session_group_id ) 
+	begin
+		set @result = 2;
+		return;
+	end;
+	if @assigned = 0
+		delete from Sesja_grupa_sesji where IdGrupa_sesji = @session_group_id and IdSesja = @session_id;
+	else
+	if not exists ( select * from	Sesja_grupa_sesji where IdGrupa_sesji = @session_group_id and IdSesja = @session_id ) 
+		insert into Sesja_grupa_sesji ( IdGrupa_sesji, IdSesja ) values ( @session_group_id, @session_id );
+end;
+go
+
+
+-- last rev. 2012-04-05
+-- Error codes:
+-- 1 session group not found
+-- 2 user group not found
+create procedure c (  @session_group_id int, @user_group_id int, @assigned bit, @result int OUTPUT)
+as
+begin
+	set @result = 0;
+	if not exists ( select * from Grupa_sesji where IdGrupa_sesji = @session_group_id ) 
+	begin
+		set @result = 1;
+		return;
+	end;
+	if not exists ( select * from Grupa_uzytkownikow where IdGrupa_uzytkownikow= @user_group_id ) 
+	begin
+		set @result = 2;
+		return;
+	end;
+	if @assigned = 0
+		delete from Grupa_sesji_grupa_uzytkownikow where IdGrupa_sesji = @session_group_id and IdGrupa_uzytkownikow = @user_group_id;
+	else
+	if not exists ( select * from	Grupa_sesji_grupa_uzytkownikow where IdGrupa_sesji = @session_group_id and IdGrupa_uzytkownikow = @user_group_id ) 
+		insert into Grupa_sesji_grupa_uzytkownikow ( IdGrupa_sesji, IdGrupa_uzytkownikow ) values ( @session_group_id, @user_group_id )
+end;
+go
