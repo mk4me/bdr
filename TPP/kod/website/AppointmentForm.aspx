b9861b4c6096c3750b93c4e558784a303d224310
WiktorF <WiktorF@5173494c-6dae-45e1-bfb1-e08744c75a4d>
2013-08-07 13:41:59 +0000

    git-svn-id: https://localhost:8443/svn/bdr@1018 5173494c-6dae-45e1-bfb1-e08744c75a4d

--- TPP/kod/website/AppointmentForm.aspx
+++ TPP/kod/website/AppointmentForm.aspx
@@ -7,7 +7,7 @@
         <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.1/i18n/jquery.ui.datepicker-pl.min.js" type="text/javascript"></script>
         <script type="text/javascript" language="javascript">
             $(document).ready(function () {
-                $("#inputDateIn").datepicker({
+                $("#<%= textDateIn.ClientID %>").datepicker({
                     showOn: 'button',
                     buttonImage: 'img/calendar_datepicker.png',
                     buttonImageOnly: true,
@@ -20,7 +20,7 @@
         </script>
         <script type="text/javascript" language="javascript">
             $(document).ready(function () {
-                $("#inputDateOut").datepicker({
+                $("#<%= textDateOut.ClientID %>").datepicker({
                     showOn: 'button',
                     buttonImage: 'img/calendar_datepicker.png',
                     buttonImageOnly: true,
@@ -36,7 +36,7 @@
     <asp:Label ID="labelPatientNumber" runat="server" Text="labelPatientNumber"></asp:Label>
     <table ID="Table1" runat="server">
         <tr>
-            <td>Typ wizyty:</td>
+            <td>Rodzaj wizyty:</td>
             <td>
                 <asp:DropDownList ID="dropAppointmentType" runat="server">
                 </asp:DropDownList>
@@ -45,13 +45,151 @@
         <tr>
             <td>Data przyjęcia:</td>
             <td>
-                <input type="text" id="inputDateIn" />
+                <asp:TextBox ID="textDateIn" runat="server"></asp:TextBox>
             </td>
         </tr>
         <tr>
             <td>Data wypisu:</td>
             <td>
-                <input type="text" id="inputDateOut" />
+                <asp:TextBox ID="textDateOut" runat="server"></asp:TextBox>
+            </td>
+        </tr>
+        <tr>
+            <td>Wykształcenie:</td>
+            <td>
+                <asp:DropDownList ID="dropEducation" runat="server">
+                </asp:DropDownList>
+            </td>
+        </tr>
+        <tr>
+            <td>Rodzinność:</td>
+            <td>
+                <asp:DropDownList ID="dropFamily" runat="server">
+                    <asp:ListItem Value="1">Tak</asp:ListItem>
+                    <asp:ListItem Value="0">Nie</asp:ListItem>
+                </asp:DropDownList>
+            </td>
+        </tr>
+        <tr>
+            <td>Miesiąc i rok zachorowania:</td>
+            <td>
+                <asp:DropDownList ID="dropMonth" runat="server">
+                </asp:DropDownList>
+                <asp:DropDownList ID="dropYear" runat="server">
+                </asp:DropDownList>
+            </td>
+        </tr>
+        <tr>
+            <td>Pierwszy objaw:</td>
+            <td>
+                <asp:DropDownList ID="dropSymptom" runat="server">
+                </asp:DropDownList>
+            </td>
+        </tr>
+        <tr>
+            <td>Czas od począku objawów do włączenia LDopy:</td>
+            <td>
+                <asp:TextBox ID="textTimeSymptom" runat="server"></asp:TextBox>
+                <asp:RegularExpressionValidator Runat="server" ID="RegularExpressionValidator1" 
+                    ControlToValidate="textTimeSymptom"
+                    ErrorMessage="Proszę podać liczbę od 0 do 255."
+                    ValidationExpression="^([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])$"></asp:RegularExpressionValidator>
+            </td>
+        </tr>
+        <tr>
+            <td>Dyskinezy obecnie:</td>
+            <td>
+                <asp:DropDownList ID="dropDiskinesia" runat="server" 
+                    onselectedindexchanged="dropDiskinesia_SelectedIndexChanged" 
+                    AutoPostBack="True">
+                    <asp:ListItem Value="1">Tak</asp:ListItem>
+                    <asp:ListItem Value="0">Nie</asp:ListItem>
+                </asp:DropDownList>
+            </td>
+        </tr>
+        <tr>
+            <td>Czas dyskinez:</td>
+            <td>
+                <asp:TextBox ID="textTimeDiskinesia" runat="server"></asp:TextBox>
+                <asp:RegularExpressionValidator ID="RegularExpressionValidator2" runat="server" 
+                    ErrorMessage="Proszę podać liczbę od 0 do 99.9."
+                    ValidationExpression="^[0-9]{1,2}(?:\.[0-9]{1})?$"
+                    ControlToValidate="textTimeDiskinesia"></asp:RegularExpressionValidator>
+            </td>
+        </tr>
+        <tr>
+            <td>Fluktuacje obecnie:</td>
+            <td>
+                <asp:DropDownList ID="dropFluctuations" runat="server"
+                    onselectedindexchanged="dropFluctuations_SelectedIndexChanged" 
+                    AutoPostBack="True">
+                    <asp:ListItem Value="1">Tak</asp:ListItem>
+                    <asp:ListItem Value="0">Nie</asp:ListItem>
+                </asp:DropDownList>
+            </td>
+        </tr>
+        <tr>
+            <td>Fluktuacje od lat:</td>
+            <td>
+                <asp:TextBox ID="textYearsFluctuations" runat="server"></asp:TextBox>
+                <asp:RegularExpressionValidator ID="RegularExpressionValidator3" runat="server" 
+                    ErrorMessage="Proszę podać liczbę od 0 do 99.9."
+                    ValidationExpression="^[0-9]{1,2}(?:\.[0-9]{1})?$"
+                    ControlToValidate="textYearsFluctuations"></asp:RegularExpressionValidator>
+            </td>
+        </tr>
+        <tr>
+            <td>Papierosy:</td>
+            <td>
+                <asp:DropDownList ID="dropCigarettes" runat="server">
+                </asp:DropDownList>
+            </td>
+        </tr>
+        <tr>
+            <td>Kawa:</td>
+            <td>
+                <asp:DropDownList ID="dropCoffee" runat="server">
+                </asp:DropDownList>
+            </td>
+        </tr>
+        <tr>
+            <td>Zielona herbata:</td>
+            <td>
+                <asp:DropDownList ID="dropGreenTea" runat="server">
+                    <asp:ListItem Value="1">Tak</asp:ListItem>
+                    <asp:ListItem Value="0">Nie</asp:ListItem>
+                </asp:DropDownList>
+            </td>
+        </tr>
+        <tr>
+            <td>Alkohol:</td>
+            <td>
+                <asp:DropDownList ID="dropAlcohol" runat="server">
+                </asp:DropDownList>
+            </td>
+        </tr>
+        <tr>
+            <td>Liczba zabiegów w znieczuleniu ogólnym przed rozpoznaniem PD:</td>
+            <td>
+                <asp:TextBox ID="textTreatmentNumber" runat="server"></asp:TextBox>
+                <asp:RegularExpressionValidator Runat="server" ID="RegularExpressionValidator4" 
+                    ControlToValidate="textTreatmentNumber"
+                    ErrorMessage="Proszę podać liczbę od 0 do 255."
+                    ValidationExpression="^([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])$"></asp:RegularExpressionValidator>
+            </td>
+        </tr>
+        <tr>
+            <td>Zamieszkanie:</td>
+            <td>
+                <asp:DropDownList ID="dropPlace" runat="server">
+                </asp:DropDownList>
+            </td>
+        </tr>
+        <tr>
+            <td>Narażenie na toksyczność:</td>
+            <td>
+                <asp:DropDownList ID="dropToxic" runat="server">
+                </asp:DropDownList>
             </td>
         </tr>
     </table>
--- TPP/kod/website/AppointmentForm.aspx.cs
+++ TPP/kod/website/AppointmentForm.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+using System.Data;
 
 public partial class AppointmentForm : System.Web.UI.Page
 {
@@ -15,7 +18,55 @@ public partial class AppointmentForm : System.Web.UI.Page
             dropAppointmentType.DataTextField = "Value";
             dropAppointmentType.DataValueField = "Key";
             dropAppointmentType.DataBind();
-            buttonOK.Enabled = false;
+
+            int minYear = 1920;
+            int currentYear = DateTime.Now.Year;
+            for (int i = minYear; i <= currentYear; i++)
+            {
+                dropYear.Items.Add(new ListItem("" + i, "" + i));
+            }
+
+            dropYear.SelectedIndex = currentYear - minYear - 10;
+
+            for (int i = 1; i <= 12; i++)
+            {
+                dropMonth.Items.Add(new ListItem("" + i, "" + i));
+            }
+
+            dropEducation.DataSource = DatabaseProcedures.getEnumerationByte("Wizyta", "Wyksztalcenie");
+            dropEducation.DataTextField = "Value";
+            dropEducation.DataValueField = "Key";
+            dropEducation.DataBind();
+
+            dropSymptom.DataSource = DatabaseProcedures.getEnumerationByte("Wizyta", "PierwszyObjaw");
+            dropSymptom.DataTextField = "Value";
+            dropSymptom.DataValueField = "Key";
+            dropSymptom.DataBind();
+
+            dropCigarettes.DataSource = DatabaseProcedures.getEnumerationByte("Wizyta", "Papierosy");
+            dropCigarettes.DataTextField = "Value";
+            dropCigarettes.DataValueField = "Key";
+            dropCigarettes.DataBind();
+
+            dropCoffee.DataSource = DatabaseProcedures.getEnumerationByte("Wizyta", "Kawa");
+            dropCoffee.DataTextField = "Value";
+            dropCoffee.DataValueField = "Key";
+            dropCoffee.DataBind();
+
+            dropAlcohol.DataSource = DatabaseProcedures.getEnumerationByte("Wizyta", "Alkohol");
+            dropAlcohol.DataTextField = "Value";
+            dropAlcohol.DataValueField = "Key";
+            dropAlcohol.DataBind();
+
+            dropPlace.DataSource = DatabaseProcedures.getEnumerationByte("Wizyta", "Zamieszkanie");
+            dropPlace.DataTextField = "Value";
+            dropPlace.DataValueField = "Key";
+            dropPlace.DataBind();
+
+            dropToxic.DataSource = DatabaseProcedures.getEnumerationByte("Wizyta", "NarazenieNaToks");
+            dropToxic.DataTextField = "Value";
+            dropToxic.DataValueField = "Key";
+            dropToxic.DataBind();
         }
 
         string patientNumber = Request.QueryString["PatientNumber"];
@@ -25,9 +76,120 @@ public partial class AppointmentForm : System.Web.UI.Page
         }
     }
 
+    private void saveExamination(string number, decimal examinationType, byte education, byte family, short symptomYear, byte symptomMonth, byte firstSymptom, byte timeSymptom,
+        byte dyskinesia, decimal timeDiskinesia, byte fluctuations, decimal yearsFluctuations, byte cigarettes, byte coffee, byte greenTea, byte alcohol, byte treatmentNumber,
+        byte place, byte toxic, DateTime dateIn, DateTime dateOut, string login, bool update)
+    {
+        SqlConnection con = new SqlConnection(WebConfigurationManager.ConnectionStrings["TPPServer"].ToString());
+        SqlCommand cmd = new SqlCommand();
+        cmd.CommandType = CommandType.StoredProcedure;
+        cmd.CommandText = "[dbo].[update_examination_questionnaire_partA]";
+        cmd.Parameters.Add("@NumerPacjenta", SqlDbType.VarChar, 20).Value = number;
+        SqlParameter examinationTypeDecimal = new SqlParameter("@RodzajWizyty", SqlDbType.Decimal);
+        examinationTypeDecimal.Precision = 2;
+        examinationTypeDecimal.Scale = 1;
+        examinationTypeDecimal.Value = examinationType;
+        cmd.Parameters.Add(examinationTypeDecimal);
+        cmd.Parameters.Add("@DataPrzyjecia", SqlDbType.Date).Value = dateIn;
+        cmd.Parameters.Add("@DataWypisu", SqlDbType.Date).Value = dateOut;
+        cmd.Parameters.Add("@Wyksztalcenie", SqlDbType.TinyInt).Value = education;
+        cmd.Parameters.Add("@Rodzinnosc", SqlDbType.TinyInt).Value = family;
+        cmd.Parameters.Add("@RokZachorowania", SqlDbType.SmallInt).Value = symptomYear;
+        cmd.Parameters.Add("@MiesiacZachorowania", SqlDbType.TinyInt).Value = symptomMonth;
+        cmd.Parameters.Add("@PierwszyObjaw", SqlDbType.TinyInt).Value = firstSymptom;
+        cmd.Parameters.Add("@CzasOdPoczObjDoWlLDopy", SqlDbType.TinyInt).Value = timeSymptom;
+        cmd.Parameters.Add("@DyskinezyObecnie", SqlDbType.TinyInt).Value = dyskinesia;
+        SqlParameter dyskinesiaDecimal = new SqlParameter("@CzasDyskinez", SqlDbType.Decimal);
+        dyskinesiaDecimal.Precision = 3;
+        dyskinesiaDecimal.Scale = 1;
+        dyskinesiaDecimal.Value = timeDiskinesia;
+        cmd.Parameters.Add(dyskinesiaDecimal);
+        cmd.Parameters.Add("@FluktuacjeObecnie", SqlDbType.TinyInt).Value = fluctuations;
+        SqlParameter fluctuationsDecimal = new SqlParameter("@FluktuacjeOdLat", SqlDbType.Decimal);
+        fluctuationsDecimal.Precision = 3;
+        fluctuationsDecimal.Scale = 1;
+        fluctuationsDecimal.Value = yearsFluctuations;
+        cmd.Parameters.Add(fluctuationsDecimal);
+        cmd.Parameters.Add("@Papierosy", SqlDbType.TinyInt).Value = cigarettes;
+        cmd.Parameters.Add("@Kawa", SqlDbType.TinyInt).Value = coffee;
+        cmd.Parameters.Add("@ZielonaHerbata", SqlDbType.TinyInt).Value = greenTea;
+        cmd.Parameters.Add("@Alkohol", SqlDbType.TinyInt).Value = alcohol;
+        cmd.Parameters.Add("@ZabiegowWZnieczOgPrzedRozpoznaniemPD", SqlDbType.TinyInt).Value = treatmentNumber;
+        cmd.Parameters.Add("@Zamieszkanie", SqlDbType.TinyInt).Value = place;
+        cmd.Parameters.Add("@NarazenieNaToks", SqlDbType.TinyInt).Value = toxic;
+        cmd.Parameters.Add("@allow_update_existing", SqlDbType.Bit).Value = update;
+        cmd.Parameters.Add("@actor_login", SqlDbType.VarChar, 50).Value = login;
+        cmd.Parameters.Add("@result", SqlDbType.Int);
+        cmd.Parameters["@result"].Direction = ParameterDirection.Output;
+        cmd.Parameters.Add("@visit_id", SqlDbType.Int);
+        cmd.Parameters["@visit_id"].Direction = ParameterDirection.Output;
+        cmd.Parameters.Add("@message", SqlDbType.VarChar, 200);
+        cmd.Parameters["@message"].Direction = ParameterDirection.Output;
+        cmd.Connection = con;
+
+        int success = 0;
+        try
+        {
+            con.Open();
+            cmd.ExecuteNonQuery();
+            success = (int)cmd.Parameters["@result"].Value;
+            if (success == 0 || success == 1)
+            {
+                Response.Redirect("~/AppointmentList.aspx?PatientNumber=" + Request.QueryString["PatientNumber"]);
+            }
+            else
+            {
+                labelMessage.Text = (string)cmd.Parameters["@message"].Value;
+            }
+        }
+        catch (SqlException ex)
+        {
+            labelMessage.Text = ex.Message;
+        }
+        finally
+        {
+            cmd.Dispose();
+            if (con != null)
+            {
+                con.Close();
+            }
+        }
+    }
+
+    protected void dropDiskinesia_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (byte.Parse(dropDiskinesia.SelectedValue) == 1)
+        {
+            textTimeDiskinesia.Enabled = true;
+        }
+        else
+        {
+            textTimeDiskinesia.Enabled = false;
+            textTimeDiskinesia.Text = "";
+        }
+    }
+
+    protected void dropFluctuations_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (byte.Parse(dropFluctuations.SelectedValue) == 1)
+        {
+            textYearsFluctuations.Enabled = true;
+        }
+        else
+        {
+            textYearsFluctuations.Enabled = false;
+            textYearsFluctuations.Text = "";
+        }
+    }
+
     protected void buttonOK_Click(object sender, EventArgs e)
     {
-
+        saveExamination(Request.QueryString["PatientNumber"], decimal.Parse(dropAppointmentType.SelectedValue), byte.Parse(dropEducation.SelectedValue), byte.Parse(dropFamily.SelectedValue),
+            short.Parse(dropYear.SelectedValue), byte.Parse(dropMonth.SelectedValue), byte.Parse(dropSymptom.SelectedValue), byte.Parse(textTimeSymptom.Text),
+            byte.Parse(dropDiskinesia.SelectedValue), decimal.Parse(textTimeDiskinesia.Text), byte.Parse(dropFluctuations.SelectedValue), decimal.Parse(textYearsFluctuations.Text),
+            byte.Parse(dropCigarettes.SelectedValue), byte.Parse(dropCoffee.SelectedValue), byte.Parse(dropGreenTea.SelectedValue), byte.Parse(dropAlcohol.SelectedValue),
+            byte.Parse(textTreatmentNumber.Text), byte.Parse(dropPlace.SelectedValue), byte.Parse(dropToxic.SelectedValue), DateTime.Parse(textDateIn.Text), DateTime.Parse(textDateOut.Text),
+            User.Identity.Name, false);
     }
 
     protected void buttonCancel_Click(object sender, EventArgs e)
